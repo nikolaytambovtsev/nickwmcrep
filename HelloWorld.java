826bd5016d193eccd68f6aa8e519f96a7e5a9b89
nikolaytambovtsev <60002192+nikolaytambovtsev@users.noreply.github.com>
2020-01-17 15:27:28 +0300

    home work1

---
+++ HelloWorld.java
@@ -0,0 +1,32 @@
+package ru.geekbrains;
+
+import javax.swing.*;
+
+public class HelloWorld{
+public static void main(String[] args);
+        int a=2;
+        int b=3;
+        int c=4;
+        int d=5;
+        float x=a*(b+(c/d));
+        System.out.println("x="x);
+        }
+public static void main(String[] args) {
+    int e = 10;
+    int f = 2;
+    int g = e + f;
+    if (20 =< g >= 10);
+    System.out.println("true");
+else
+    System.out.println("false")
+}
+    public static void main(String[] args){
+    int h = 3;
+    if (h>=0);
+System.out.println("положительное число");
+else System.out.println("отрицательное число");
+    }
+    public static void main(String[] args) {
+        String i = "NickW";
+        System.out.print("привет" + i)
+    }
